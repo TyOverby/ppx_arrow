a29b1afde83a9ba3c0a98fce01005c1531297998
Ty Overby <ty@pre-alpha.com>
2020-03-09 02:56:05 +0000

    _

--- ppx/arrow_ppx.ml
+++ ppx/arrow_ppx.ml
@@ -34,34 +34,32 @@ let rec fetch_lets ~let_loc expr =
     |> map2 ~f:(fun ident arrow -> ident, arrow)
     |> map2 ~f:(fun (ident, arrow) arg -> ident, arrow, arg)
     |> map2 ~f:(fun (ident, arrow, arg) cont ->
-        let arg = Longident.last_exn arg in
-        let this = M.Statement.Arrow_let {ident; arrow; arg} in
-        let rest, last = fetch_lets ~let_loc:(Location.none) cont in 
-        this::rest, last)
+           let arg = Longident.last_exn arg in
+           let this = M.Statement.Arrow_let { ident; arrow; arg } in
+           let let_loc = cont.pexp_loc in
+           let rest, last = fetch_lets ~let_loc cont in
+           this :: rest, last)
   in
   let regular_let =
     let_pattern __
     |> map2 ~f:(fun ident expr -> ident, expr)
     |> map2 ~f:(fun (ident, expr) cont ->
-        let this = M.Statement.Regular_let [{M.Statement.ident; expr}] in 
-        let rest, last = fetch_lets ~let_loc:(Location.none) cont in 
-        this :: rest, last)
+           let this =
+             M.Statement.Regular_let [ { M.Statement.ident; expr } ]
+           in
+           let let_loc = cont.pexp_loc in
+           let rest, last = fetch_lets ~let_loc cont in
+           this :: rest, last)
   in
-  let other_expression = 
-    (__ ) |> map1 ~f:(fun expr -> [], expr) in
-  Ast_pattern.parse (arrow_let ||| regular_let ||| other_expression) let_loc expr (fun x -> x)
+  let other_expression = __ |> map1 ~f:(fun expr -> [], expr) in
+  Ast_pattern.parse
+    (arrow_let ||| regular_let ||| other_expression)
+    let_loc
+    expr
+    Fn.id
 ;;
 
-let expand ~loc ~path:_ (initial_input : pattern) (body : expression) =
-  let initial_identifier, _loc =
-    match initial_input with
-    | { ppat_desc = Ppat_var { txt; loc }; _ } -> txt, loc
-    | _ ->
-      Location.raise_errorf
-        ~loc
-        "only plain identifier patterns are allowed for the wrapping \
-         function for an arrow-expression"
-  in
+let expand ~loc ~path:_ (initial_identifier) (body : expression) =
   let initial_input = M.Identifier.of_string initial_identifier in
   let lets, final_expression = fetch_lets ~let_loc:loc body in
   M.transform ~initial_input ~lets ~final_expression
@@ -71,7 +69,7 @@ let ext =
   Extension.declare
     "arrow"
     Extension.Context.expression
-    Ast_pattern.(single_expr_payload (pexp_fun nolabel none __ __))
+    Ast_pattern.(single_expr_payload (pexp_fun nolabel none (ppat_var __) __))
     expand
 ;;
 
