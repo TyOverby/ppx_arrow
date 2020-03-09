74dc3316a1d67404bb6e1189d09d8f0274b96ce2
Ty Overby <ty@pre-alpha.com>
2020-03-09 02:11:52 +0000

    _

--- ppx_test/test.ml
+++ ppx_test/test.ml
@@ -1,14 +1,13 @@
-let k = [%arrow (fun a -> 
-    let x = a + 1 in 
-    let y = a + x in 
+let k = [%arrow (fun a ->
+    let x = a + 1 in
+    let y = a + x in
     y
 )]
 
-let s = [%arrow (fun a -> 
- let x = a + 1 in 
- let y = a + x in  
+let s = [%arrow (fun a ->
+ let x = a + 1 in
+ let y = a + x in
  let w = [%call f x] in
  let z = y + w in
  z
 )]
-
